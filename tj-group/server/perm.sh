bf5a2d74de0d2dcc5e1a96c457b57b2ec199d3bd
zf29 <zf29@0e6d0bca-c5e7-0410-ba95-cdce8a16c6d0>
2014-04-30 16:43:17 +0000

    Added perm.sh

---
+++ tj-group/server/perm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Running python script"
+
+/usr/local/epd/bin/python parser_vFinal.py
+
+
+echo "Touching new year files"
+
+ls /data/coaccess/round5/2014 > cur.txt
+
+
+#cat cur.txt pastyearUniq.txt > newyears.txt
+
+
+# final merge
+#sort -u newyears.txt > newyearsUniq.txt
+
+cd /data/coaccess/round5
+mkdir -m 777 newyear
+cd newyear
+cp /data/coaccess/round5/cur.txt /data/coaccess/round5/newyear
+cd /data/coaccess/round5/newyear
+
+
+while read p; do touch $p; done < cur.txt
+
+rm cur.txt
+
+
+echo "Creating Lucene Index"
+
+# Lucene indexing...
+cd /data/coaccess/round5/lucene_framework
+java -cp lucene-analyzers-common-4.5.1.jar:lucene-demo-4.5.1-SNAPSHOT.jar:lucene-core-4.5.1.jar:lucene-queryparser-4.5.1.jar org.apache.lucene.demo.IndexFiles -docs /data/coaccess/round5/final/
+
+
+cd /data/coaccess/round5
+
+
+echo "Removing .txt files..."
+
+rm *.txt
+
+
+
